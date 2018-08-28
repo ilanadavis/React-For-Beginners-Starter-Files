a1fbbf36c4fba01047754a8bd28108ff712f55ee
Ilana Davis <ilana@ilanadavis.com>
2018-08-28 22:30:39 +0000

    set up of project

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react';  //importing the entire react library
+import {  render } from 'react-dom'; //importing a specific method
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hello!</p>
+    }
+}
+
