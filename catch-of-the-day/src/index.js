d9b3d1ac958fc7898fbb1d190608b08e5f60d1ab
Ilana Davis <ilana@ilanadavis.com>
2018-08-29 22:08:40 +0000

    add storepicker class and import css file

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';  //importing the entire react library
 import {  render } from 'react-dom'; //importing a specific method
 import StorePicker from './components/StorePicker';
-
+import './css/style.css';
 
 render(<StorePicker />, document.querySelector('#main'));
